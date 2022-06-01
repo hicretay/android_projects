64e9429520f374ae50c8902c60210686f4c473ce
Hicret AY <46728765+hicrett@users.noreply.github.com>
2022-06-01 23:51:38 +0300

    Delete yasam_suresi_uygulamasi directory

--- yasam_suresi_uygulamasi/android/app/src/main/java/com/example/yasam_suresi_uygulamasi/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.yasam_suresi_uygulamasi;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- yasam_suresi_uygulamasi/lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-TextStyle kMetinStili =
-    TextStyle(color: Colors.black54, fontSize: 20, fontWeight: FontWeight.bold);
-
-TextStyle kSayiStili = TextStyle(
-    color: Colors.lightBlue, fontSize: 35, fontWeight: FontWeight.bold);
