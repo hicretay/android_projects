911a18f54e018280cca4d815f78997cd49b572a7
Hicret AY <46728765+hicrett@users.noreply.github.com>
2022-06-01 23:51:20 +0300

    Delete bilgi_testi_uygulamasi directory

--- bilgi_testi_uygulamasi/android/app/src/main/java/com/example/bilgi_testi_uygulamasi/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.bilgi_testi_uygulamasi;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- bilgi_testi_uygulamasi/lib/soru.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Soru {
-  String soruMetni;
-  bool soruYaniti;
-
-  Soru({@required this.soruMetni, @required this.soruYaniti}); // constructor
-}
